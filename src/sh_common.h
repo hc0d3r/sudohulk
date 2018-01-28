c76037c4f5b880a1a8fc4c0ce77a2de05274544d
MMxM <mmedeiros450@gmail.com>
2018-01-28 21:42:48 -0200

    update #error message

--- src/sh_common.h
+++ src/sh_common.h
@@ -2,7 +2,7 @@
 #define __SH_COMMON__
 
 #if !defined (__x86_64__) && !defined (__i386__)
-	#error "only works in x86_64"
+	#error "only works in x86_64 and x86"
 #endif
 
 #define __wordsize sizeof(long)
